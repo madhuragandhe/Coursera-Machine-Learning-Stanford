bbe6bb6373ba4c8a1656cdfe329c27df99e906d5
Madhura Gandhe <43580898+madhuragandhe@users.noreply.github.com>
2020-07-20 17:07:55 +0530

    Update linearRegCostFunction.m

--- Week 6/Programming Assignment/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ Week 6/Programming Assignment/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -12,18 +12,24 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-m = length(y); % number of training examples
+h_x=X*theta;
+J=(1/(2*m))* sum((h_x-y).^2) + (lambda/(2*m))*sum(theta(2:end).^2);
+grad(1) = (1/m)*(X(:,1)'*(h_x-y)); 
+grad(2:end) = (1/m)*(X(:,2:end)'*(h_x-y)) + (lambda/m)*theta(2:end);
 
-h_theta = X * theta;
 
-J = 1/(2*m) * (h_theta - y)' * (h_theta - y) + ...
-    (lambda/(2*m)) * (theta(2:length(theta)))' * theta(2:length(theta));
+% m = length(y); % number of training examples
 
-thetaZero = theta;
-thetaZero(1) = 0;
+% h_theta = X * theta;
 
-grad = ((1 / m) * (h_theta - y)' * X) + ...
-    lambda / m * thetaZero';
+% J = 1/(2*m) * (h_theta - y)' * (h_theta - y) + ...
+%     (lambda/(2*m)) * (theta(2:length(theta)))' * theta(2:length(theta));
+
+% thetaZero = theta;
+% thetaZero(1) = 0;
+
+% grad = ((1 / m) * (h_theta - y)' * X) + ...
+%     lambda / m * thetaZero';
 
 grad = grad(:);
 
